b6a535571511a69aed16d7ec5de407422fef1900
tzzhmm <tzzhmm@gmail.com>
2022-07-19 14:52:26 +0800

    修改配置文件

--- vue.config.js
+++ vue.config.js
@@ -10,7 +10,7 @@ module.exports = {
     devServer: {
         host: "0.0.0.0",
         // 端口号
-        port: 8080,
+        port: 9870,
         https: false,
         // https:{type:Boolean}
         //配置自动启动浏览器
@@ -18,7 +18,7 @@ module.exports = {
         //热更新
         hot: true,
         //配置多个跨域  如果遇到options.proxy should be {Object|Array}错误,将改为  proxy:null
-        proxy: { }
+        proxy: null
         // proxy: proxyConfig.proxyList,
     },
 };

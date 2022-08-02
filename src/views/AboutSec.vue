2a52c87750f28528f37db3405cc12c8adb17fa82
tzzhmm <tzzhmm@gmail.com>
2022-08-02 15:06:08 +0800

    add remark

--- src/views/AboutSec.vue
+++ src/views/AboutSec.vue
@@ -62,6 +62,12 @@ export default class HelloWorld extends Vue {
 	 * @Emit 装饰器接收一个可选参数，该参数是[公式]Emit 会将回调函数名的 camelCase 转为 kebab-case，并将其作为事件名
 	 * @Emit 会将回调函数的返回值作为第二个参数，如果返回值是一个 Promise 对象，$emit 会在 Promise 对象被标记为 resolved 之后触发
 	 * @Emit 的回调函数的参数，会放在其返回值之后，一起被$emit 当做参数使用
+	 * 
+	 * 注意：
+		如果使用@Emit(event?: string) 没有传递事件名称,父类中事件名称会被转为小写用-连接，如例子中的 @add-to-count。
+		也有人不太喜欢用小写的这种,如例子中的 @add-to-count,那在子类@Emit中传递事件名,父类用传递的事件名接收。如：子类中@Emit("addToCount") 父类@addToCount="addToCount"
+	 * 
+	 * 
 	**/
 	@Emit()
     addToCount(n: number) {
